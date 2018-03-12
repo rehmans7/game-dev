41ffe62927fc0ac7f92bccd6ec2d7a74e1396234
Sabah Rehman <rehmans7@aston.ac.uk>
2018-03-12 01:46:59 +0000

    gun

---
+++ Coursework/Assets/Scripts/GunFire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GunFire : MonoBehaviour {
+
+    public AudioSource gunFireSound;
+
+	// Use this for initialization
+	void Start () {
+        gunFireSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gunFireSound.Play();
+           // GetComponent.<Animation>().Play("Shoot");
+        }
+    }
+}
---
+++ Coursework/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame () {
+        SceneManager.LoadSceneAsync(1);
+	}
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
